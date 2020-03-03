1821a46ce9700766e946b03d4683090aa8de21b4
Anthony Kawa <anthonyk2020@gmail.com>
2020-03-03 08:00:33 -0800

    added gatsby styled components and babel styled components to dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,15 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Change plugin default options here, e.g.:
+        // ssr: false
+        // displayName: false, 
+        // minify: false
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
