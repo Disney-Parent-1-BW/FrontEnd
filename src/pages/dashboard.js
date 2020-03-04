a36aec8fdccb89105d02e5a82b3a78d0670e58a0
Jeffrey Desir <jeffreyjdesir@gmail.com>
2020-03-04 13:19:56 -0500

    added: action and store

---
+++ src/pages/dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Layout } from "antd";
+
+
+const Dashboard = ({ children }) => {
+
+    return(
+        <Layout>
+
+        </Layout>
+    )
+}
+
+export default Dashboard;
